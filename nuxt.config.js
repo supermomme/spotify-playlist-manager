dfaf7747cf0e14fb43bd5151a669d758646dd973
Momme <momme@juergensen.me>
2018-05-08 14:06:43 +0200

    big bang

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+const BabiliPlugin = require('babili-webpack-plugin')
+
+module.exports = {
+  srcDir: 'client/',
+  router: {
+    middleware: []
+  },
+  plugins: [
+    //{ src: '~/plugins/nuxt-client-init.js', ssr: false },
+    //{ src: '~/plugins/moment.js' }
+  ],
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'spotify-playlist-manager',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/scss/main.scss'
+  ],
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: [],
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (!ctx.isDev) {
+        config.plugins = config.plugins.filter((plugin) => plugin.constructor.name !== 'UglifyJsPlugin')
+        config.plugins.push(new BabiliPlugin())
+      }
+
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,42 @@
+import { Nuxt, Builder } from 'nuxt'
+
+const feathers = require('@feathersjs/feathers')
+const express = require('@feathersjs/express')
+const app = express(feathers())
+const socketio = require('@feathersjs/socketio')
+const logger = require('winston')
+const api = require('./app')
+const port = api.get('port')
+const host = api.get('host')
+
+app.set('port', port)
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+// Init Nuxt.js
+const nuxt = new Nuxt(config)
+
+// Build only in dev mode
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build()
+}
+
+// Give nuxt middleware to express
+app.configure(socketio({
+  path: '/api/socket.io/'
+}))
+
+app.use('/api', api)
+app.use(nuxt.render)
+
+// Listen the server
+let server = app.listen(port, host)
+logger.info('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+api.setup(server)
+
+process.on('unhandledRejection', (reason, p) =>
+  logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
